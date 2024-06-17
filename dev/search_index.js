95ec239bfbfdcae4e5e1e2d83d5dc9ed8e7a6dcf
Documenter.jl <documenter@juliadocs.github.io>
2024-06-17 18:00:31 +0000

    build based on f11965f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = uniqmat","category":"page"},{"location":"#uniqmat","page":"Home","title":"uniqmat","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for uniqmat.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [uniqmat]","category":"page"}]
+}
